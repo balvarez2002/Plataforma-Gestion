84ada80bdae152e14f07edfca127bf14dc765488
balvarez2002 <129675483+balvarez2002@users.noreply.github.com>
2024-10-08 16:09:03 -0500

    Add files via upload

---
+++ script_combined.js
@@ -0,0 +1,6 @@
+// Script que maneja la funcionalidad dinámica de la página
+
+// Combined JavaScript
+console.log('Combined JavaScript file loaded.');
+
+// Additional JS from other files can be appended here
